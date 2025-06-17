3b61be40a9cb2600d97b4b97150f25182c053f87
Your Name <jetendrasinghratore1@gmail.com>
2025-06-17 15:59:33 +0530

    google analytic code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,26 @@ import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 import './globals.css';
 import FloatingContact from '@/components/FloatingContact';
-
-
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5JWJXL7WNE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5JWJXL7WNE');
+          `}
+        </Script>
+
         <Navbar />
         <main>{children}</main> 
         <FloatingContact/>
